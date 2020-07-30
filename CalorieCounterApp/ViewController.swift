8322a53a2eea3cf2ce0e1bf6bcb99e3f591397b1
AbidaMohd <s3811920@student.rmit.edu.au>
2020-07-30 16:47:12 +1000

    Initial Commit

---
+++ CalorieCounterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalorieCounterApp
+//
+//  Created by Abida Mohammadi on 30/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
