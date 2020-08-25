060d3ca1b870b72c1a7cab98493bf22f122b3e68
Joshua Barua <joshua.k.barua@gmail.com>
2020-08-25 19:41:00 +0200

    Renamed "outlets", removed connections, ready for implementation. Auto-Layouts are still need correction...

--- CalorieCounterApp/ViewModel/ViewController.swift
+++ CalorieCounterApp/ViewModel/ViewController.swift
@@ -9,13 +9,13 @@ class ViewController: UIViewController {
     
     @IBOutlet var options: [UIButton]!
     
-    func configureButtons(){
-        for button in options {
-            button.layer.cornerRadius = 20.8
-        }
-    }
+   // func configureButtons(){
+    //    for button in options {
+     //       button.layer.cornerRadius = 20.8
+      //  }
+   // }
 
-    @IBOutlet weak var nameField: UITextField!
+   /* @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var dobField: UITextField!
     @IBOutlet weak var weightField: UITextField!
     
@@ -26,18 +26,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var femaleTapped: UIButton!
     
     
-    @IBAction func nextPressed1(_ sender: UIButton) {
+   @IBAction func nextPressed1(_ sender: UIButton) {
         
         let inputName = String(nameField.text!)
         let inputdob  = Double(dobField.text!)
         let inputCurrentWeight  = Double(weightField.text!)
         let inputCurrentHeight  = Double(heightField.text!)
         
-    }
+    }*/
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureButtons()
+     //   configureButtons()
     }
 
 }
