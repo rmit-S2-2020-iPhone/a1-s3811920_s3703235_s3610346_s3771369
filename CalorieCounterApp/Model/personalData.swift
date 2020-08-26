6e28af0fc6c3d6573455ee5afb29b62c385e7b1b
Timal Pathirana <s3703235@student.rmit.edu.au>
2020-08-26 11:12:35 +1000

    Created the ViewController files for user info.storyboard

---
+++ CalorieCounterApp/Model/personalData.swift
@@ -0,0 +1,43 @@
+//
+//  personalData.swift
+//  CalorieCounterApp
+//
+//  Created by Timal Pathirana on 25/8/20.
+//
+
+import Foundation
+import CoreData
+
+
+
+
+
+
+
+
+struct CustomDate{
+    // Marked as optional as invalid construction data will produce a nil value
+    private var date:Date?
+    
+    init(day:Int, month:Int, year:Int, timeZone:TimeZone = .current){
+        var dateComponents = DateComponents()
+        dateComponents.year = year
+        dateComponents.month = month
+        dateComponents.timeZone = timeZone
+    
+        
+        let userCalendar = Calendar.current
+        // Swift API here can return an Optional Value
+        date = userCalendar.date(from: dateComponents)
+    }
+    
+    // Default value if the user does not provide a format
+    func formatted(as format: String = "dd-MM-yyyy") -> String?
+    {
+        // Early termination if date has not been initialised
+        guard let date = self.date else {return nil}
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        return dateFormatter.string(from: date)
+    }
+}
---
+++ CalorieCounterApp/ViewModel/personalInfoViewController.swift
@@ -0,0 +1,56 @@
+//
+//  personalInfoViewController.swift
+//  CalorieCounterApp
+//
+//  Created by Timal Pathirana on 25/8/20.
+//
+
+import UIKit
+
+class personalInfoViewController: UIViewController {
+
+    
+    
+   
+   
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var dobField: UITextField!
+    @IBOutlet weak var currentWeight: UITextField!
+    
+    @IBOutlet weak var currentHeight: UITextField!
+    
+    
+    
+    @IBOutlet weak var genderButtonPressed: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+//  func configureUI() {
+//
+    // let image = UIImage(named: "image")
+   // let imageFilled = UIImage(named: "image-filled")
+    
+//    genderButtonPressed.backgroundColor
+//
+//
+//       genderButtonPressed.setImage(imageFilled, for: .selected)
+ //  }
+//
+//    @IBAction func someBtnPressed(_ sender: Any) {
+//        // Toggle basically makes someBtn’s selected state either true or false when pressed
+//        someBtn.isSelected.toggle()
+//    }
+//
+    
+    
+    
+}
