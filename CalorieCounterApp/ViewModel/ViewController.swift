dfd7a7a1e69790bf7e9f7fe2dea6d34b82d55afe
Timal Pathirana <s3703235@student.rmit.edu.au>
2020-08-26 21:50:11 +1000

    revert the previous push

--- CalorieCounterApp/ViewModel/ViewController.swift
+++ CalorieCounterApp/ViewModel/ViewController.swift
@@ -6,37 +6,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var options: [UIButton]!
-
+    
     func configureButtons(){
-
-            for button in options {
+        for button in options {
             button.layer.cornerRadius = 20.8
         }
-    
     }
 
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var dobField: UITextField!
     @IBOutlet weak var currentWeightLabel: UITextField!
-
+    
     @IBOutlet weak var currentHeightLabel: UITextField!
-
+    
     @IBOutlet weak var maleTapped: UIButton!
-
+    
     @IBOutlet weak var femaleTapped: UIButton!
-
-
+    
+    
     @IBAction func nextPressed1(_ sender: UIButton) {
-
+        
         let inputName = String(nameField.text!)
         let inputdob  = Double(dobField.text!)
         let inputCurrentWeight  = Double(currentWeightLabel.text!)
         let inputCurrentHeight  = Double(currentHeightLabel.text!)
-
+        
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureButtons()
