ee623243161b848390c99fe9f6fa8ec872e0f42f
Joshua Barua <joshua.k.barua@gmail.com>
2020-08-26 13:43:13 +0200

    Revert "commit"
    This reverts commit 70abfe4c6213c6e88a8465868196ab9be6527ec3.

--- CalorieCounterApp/ViewModel/ViewController.swift
+++ CalorieCounterApp/ViewModel/ViewController.swift
@@ -6,43 +6,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var options: [UIButton]!
-<<<<<<< HEAD
     
    // func configureButtons(){
     //    for button in options {
      //       button.layer.cornerRadius = 20.8
       //  }
    // }
-=======
-
-    func configureButtons(){
-
-            for button in options {
-            button.layer.cornerRadius = 20.8
-        }
-    
-    }
->>>>>>> 6e28af0fc6c3d6573455ee5afb29b62c385e7b1b
 
    /* @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var dobField: UITextField!
-<<<<<<< HEAD
     @IBOutlet weak var weightField: UITextField!
     
     @IBOutlet weak var heightField: UITextField!
     
-=======
-    @IBOutlet weak var currentWeightLabel: UITextField!
-
-    @IBOutlet weak var currentHeightLabel: UITextField!
-
->>>>>>> 6e28af0fc6c3d6573455ee5afb29b62c385e7b1b
     @IBOutlet weak var maleTapped: UIButton!
-
+    
     @IBOutlet weak var femaleTapped: UIButton!
-<<<<<<< HEAD
     
     
    @IBAction func nextPressed1(_ sender: UIButton) {
@@ -54,19 +35,6 @@ class ViewController: UIViewController {
         
     }*/
     
-=======
-
-
-    @IBAction func nextPressed1(_ sender: UIButton) {
-
-        let inputName = String(nameField.text!)
-        let inputdob  = Double(dobField.text!)
-        let inputCurrentWeight  = Double(currentWeightLabel.text!)
-        let inputCurrentHeight  = Double(currentHeightLabel.text!)
-
-    }
-
->>>>>>> 6e28af0fc6c3d6573455ee5afb29b62c385e7b1b
     override func viewDidLoad() {
         super.viewDidLoad()
      //   configureButtons()
