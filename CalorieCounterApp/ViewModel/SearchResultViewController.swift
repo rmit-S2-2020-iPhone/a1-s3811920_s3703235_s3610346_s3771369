5941387dc71b2be18d2109f8925f09d0c6ad1262
AbidaMohd <s3811920@student.rmit.edu.au>
2020-08-30 13:29:57 +1000

    searchBar-final

--- CalorieCounterApp/ViewModel/SearchResultViewController.swift
+++ CalorieCounterApp/ViewModel/SearchResultViewController.swift
@@ -17,15 +17,9 @@ class SearchResultViewController: UITableViewController, UISearchBarDelegate {
     private var mealsListViewModel: MealsListViewModel!
     private var mealData: MockData!
     
-    // searchBar filter MODEL
+    // searchBar filter
     private var filterMealsListViewModel: MealsListViewModel!
     private var filterMealData: MockData!
-    
-    
-    // searchBar filter
-//    private var meals = [Meal]()
-//    private var mealNames = [String]()
-//    private var filter = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,22 +28,16 @@ class SearchResultViewController: UITableViewController, UISearchBarDelegate {
         self.mealData = MockData()
         self.mealsListViewModel = MealsListViewModel(mealData: self.mealData)
         
+        // searchBar filter
         self.filterMealData = MockData()
         self.filterMealsListViewModel = MealsListViewModel(mealData: self.filterMealData)
-        
-        // searchBar filter
-//        self.meals = mealData.getAllMeals()
-//        mealNames = meals.map { $0.name }
-        //filter = mealNames
         
         self.tableView.reloadData()
     }
     
     // returns number of rows
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       // return self.mealsListViewModel.mealViewModel.count
         return self.filterMealsListViewModel.filterViewModel.count
-        //return filter.count
     }
     
     
@@ -57,34 +45,15 @@ class SearchResultViewController: UITableViewController, UISearchBarDelegate {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-//        let mealViewModel = self.mealsListViewModel.mealViewModel[indexPath.row]
-//        cell.textLabel?.text = "\(mealViewModel.mealName!)"
-        
-        
         let mealViewModel = self.filterMealsListViewModel.filterViewModel[indexPath.row]
         cell.textLabel?.text = "\(mealViewModel.mealName!)"
-        
-        
-        //cell.textLabel?.text = filter[indexPath.row]
+
         return cell
     }
     
     
     // search bar filter
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-//        filter = []
-//        if searchText == "" {
-//            //filter = mealNames
-//            filter = []
-//        } else {
-//            for name in mealNames {
-//                if name.lowercased().contains(searchText.lowercased()){
-//                    filter.append(name)
-//                }
-//            }
-//        }
-            
-        
         filterMealsListViewModel.filterViewModel = []
         for meal in mealsListViewModel.mealViewModel {
             if meal.mealName.lowercased().contains(searchText.lowercased()){
